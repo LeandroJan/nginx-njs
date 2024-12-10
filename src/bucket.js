928ad045d6ab64eb9d144e585bc8c9040237bab1
Leandro Jan <nt84h@gmail.com>
2024-12-10 18:46:59 -0300

    modifing the nginx to access s3 bucket

---
+++ src/bucket.js
@@ -0,0 +1,14 @@
+// Importing necessary modules
+function handle(r) {
+    var bucket = "my-static-html"; // Replace with your MinIO bucket name
+    var s3_host = "minio-service:9000"; // MinIO service in OpenShift
+    var key = r.uri.substr(1); // Strip leading "/"
+
+    // Construct the S3 URL
+    var s3_url = `http://${s3_host}/${bucket}/${key}`;
+
+    r.headersOut["Host"] = s3_host; // MinIO expects Host headers
+    r.return(302, s3_url); // Redirect client to S3 URL
+}
+
+export default { handle };
