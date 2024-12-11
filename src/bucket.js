5fa41ec3b7d191b2666f0a4a9d4fae659a987743
Leandro Jan <nt84h@gmail.com>
2024-12-11 09:32:36 -0300

    changed bucket.js to reflect the nginx as a proxy

--- src/bucket.js
+++ src/bucket.js
@@ -1,14 +1,20 @@
-// Importing necessary modules
-function handle(r) {
-    var bucket = "test"; // Replace with your MinIO bucket name
-    var s3_host = "minio-service:9000"; // MinIO service in OpenShift
-    var key = r.uri.substr(1); // Strip leading "/"
+async function handle(r) {
+    var bucket = "my-static-html"; // Replace with your MinIO bucket name
+    var s3_host = "minio-service:9000"; // MinIO service within OpenShift
+    var key = r.uri === "/" ? "index.html" : r.uri.substr(1); // Serve index.html by default
 
     // Construct the S3 URL
     var s3_url = `http://${s3_host}/${bucket}/${key}`;
 
-    r.headersOut["Host"] = s3_host; // MinIO expects Host headers
-    r.return(302, s3_url); // Redirect client to S3 URL
+    // Fetch the object from MinIO
+    r.subrequest(s3_url, function (res) {
+        if (res.status === 200) {
+            r.headersOut["Content-Type"] = "text/html"; // Set HTML content type
+            r.return(res.status, res.responseBody);
+        } else {
+            r.return(404, "File not found"); // Return a 404 if the object is missing
+        }
+    });
 }
 
 export default { handle };
