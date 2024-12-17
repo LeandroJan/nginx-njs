80ee97ea21a1c621abffd14d541cd32672449efa
Leandro Jan <nt84h@gmail.com>
2024-12-17 15:39:27 -0300

    changed redirect.js

--- src/redirect.js
+++ src/redirect.js
@@ -1,8 +1,8 @@
 function handle(r) {
     var key = r.uri === "/" ? "index.html" : r.uri.substr(1);
 
-    // Perform an internal subrequest to fetch content
-    r.internalRedirect(`@minio_proxy${key}`);
+    // Perform an internal redirect to the named location
+    r.internalRedirect(`@minio_proxy/${key}`);
 }
 
 export default { handle };
