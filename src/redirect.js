52af4bc5c8dc11086a3b21da49d73f3c4739bc10
Leandro Jan <nt84h@gmail.com>
2024-12-17 15:48:40 -0300

    changed redirect.js and nginx.conf

--- src/redirect.js
+++ src/redirect.js
@@ -1,8 +1,8 @@
 function handle(r) {
     var key = r.uri === "/" ? "index.html" : r.uri.substr(1);
 
-    // Perform an internal redirect to the named location
-    r.internalRedirect(`@minio_proxy/${key}`);
+    // Redirect to the named location with the path dynamically appended
+    r.internalRedirect(`@minio_proxy/test/${key}`);
 }
 
 export default { handle };
